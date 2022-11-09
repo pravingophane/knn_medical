38fe75be7f23da3d85d4a21663abae7a51abce0d
unknown <pravingophane@gmail.com>
2022-11-09 21:19:17 +0530

    added configuration file

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+CSV_FILE_PATH = "Enter_path"
+
+PORT_NUMBER = 2010
+
+MODEL_FILE_PATH = os.path.join("Medical_insurance","knn_model.pkl")
+
+SCALAR_FILE_PATH = os.path.join("Medical_insurance","Scalar.pkl")
+
+JSON_FILE_PATH = os.path.join("Medical_insurance","project_data.json")
